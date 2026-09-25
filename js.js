d3321e975f784d150751a7d5e0e6898789fd3fce
Axrena <93066943+Axvmc@users.noreply.github.com>
2026-09-25 22:10:30 +0700

    Delete js.js

--- js.js
+++
@@ -1,8 +0,0 @@
-// Ambil elemen judul "Profile" dan list
-const profileTitle = document.querySelector('.profile-title');
-const nameList = document.querySelector('.Name');
-
-// Event klik untuk toggle list dengan animasi
-profileTitle.addEventListener('click', () => {
-  nameList.classList.toggle('show');
-});
